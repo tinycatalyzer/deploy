ae1372cf037d0413166f70dc7f37e6f4e6a33ea1
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-18 13:18:36 -0800

    modify for catalyzer

--- gce/dns/pull.sh
+++ gce/dns/pull.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 # Updates the DNS zone based on the ./*-zone files in this repo
-gcloud dns record-sets export ./zone-opencura --zone=opencura-zone
-gcloud dns record-sets export ./zone-wbstack --zone=wbstack-zone
+gcloud dns record-sets export ./zone-catalyzer --zone=catalyzer-zone
