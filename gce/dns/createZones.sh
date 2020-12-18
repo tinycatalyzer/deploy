bba5f72be985785f99029419b53819ac43b2505a
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-18 13:17:45 -0800

    modify for catalyzer

--- gce/dns/createZones.sh
+++ gce/dns/createZones.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-gcloud deployment-manager deployments create managedzone-wbstack --config ./managedzone-wbstack.yaml
-gcloud deployment-manager deployments create managedzone-opencura --config ./managedzone-opencura.yaml
+gcloud deployment-manager deployments create managedzone-catalyzer --config ./managedzone-catalyzer.yaml
