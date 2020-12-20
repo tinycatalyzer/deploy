0c2fd431068aa18d9b6f5ff6009629e916d1dc93
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-19 20:08:16 -0800

    Update apply.sh

--- gce/snapshots/apply.sh
+++ gce/snapshots/apply.sh
@@ -4,4 +4,4 @@
 PVC=$(kubectl get pvc data-sql-mariadb-master-0 -o jsonpath="{.spec.volumeName}")
 DISK=$(gcloud compute disks list --filter="${PVC}" --format='get(name)')
 LOCATION=$(gcloud compute disks list --filter="${PVC}" --format='value(location())')
-gcloud compute disks add-resource-policies ${DISK} --zone=${LOCATION} --resource-policies=wbstack-nightly-east-to-west-2d-1
+gcloud compute disks add-resource-policies ${DISK} --zone=${LOCATION} --resource-policies=catayzer-nightly-east-to-west-2d-1
