16b8cdb280abfa692bb5f9f96601f4e639f9a4b6
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-19 19:39:53 -0800

    Update update.sh

--- gce/serviceaccounts/update.sh
+++ gce/serviceaccounts/update.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 gcloud deployment-manager deployments update serviceaccount-certman-dns01-solver --async --config ./serviceaccount.certman-dns01-solver.yaml
-gcloud deployment-manager deployments update serviceaccount-bucket-writer --async --config ./bucket-writer.yaml
-gcloud deployment-manager deployments update serviceaccount-wbstack-api --async --config ./wbstack-api.yaml
+#gcloud deployment-manager deployments update serviceaccount-bucket-writer --async --config ./bucket-writer.yaml
+gcloud deployment-manager deployments update serviceaccount-wbstack-api --async --config ./catalyzer-api.yaml
