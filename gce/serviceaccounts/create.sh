e5895b3db434e6a14202bc6b48ff88ff62a305de
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-19 19:44:32 -0800

    corrected name

--- gce/serviceaccounts/create.sh
+++ gce/serviceaccounts/create.sh
@@ -2,4 +2,4 @@
 
 gcloud deployment-manager deployments create serviceaccount-certman-dns01-solver --config ./serviceaccount.certman-dns01-solver.yaml
 #gcloud deployment-manager deployments create serviceaccount-bucket-writer --config ./bucket-writer.yaml
-gcloud deployment-manager deployments create serviceaccount-wbstack-api --config ./wbstack-api.yaml
+gcloud deployment-manager deployments create serviceaccount-wbstack-api --config ./catalyzer-api.yaml
