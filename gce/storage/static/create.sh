471fb91f577b70217cbe7d26cc3155e77a9427ef
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-19 20:30:55 -0800

    Update create.sh

--- gce/storage/static/create.sh
+++ gce/storage/static/create.sh
@@ -3,5 +3,5 @@
 # Create a static bucket for sharing between a few services with public web access..
 # Files accessible at for example: https://storage.googleapis.com/wbstack-static/file.txt
 
-gsutil.cmd mb -p wbstack -c STANDARD -l US-EAST1 -b on gs://wbstack-static/
-gsutil.cmd iam ch allUsers:objectViewer gs://wbstack-static/
+gsutil.cmd mb -p catalyzer-wikistack -c STANDARD -l US-EAST1 -b on gs://catalyzer-wikistack-static/
+gsutil.cmd iam ch allUsers:objectViewer gs://catalyzer-wikistack-static/
