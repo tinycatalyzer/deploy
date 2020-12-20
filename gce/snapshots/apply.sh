f829b3153574794dce1e9ae2d9462db26054451c
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-19 21:19:55 -0800

    Update apply.sh

--- gce/snapshots/apply.sh
+++ gce/snapshots/apply.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Apply the 2 day snapshot schedule to the catalyzer-wikistack SQL master
-PVC=$(kubectl get pvc data-sql-mariadb-master-0 -o jsonpath="{.spec.volumeName}")
-DISK=$(gcloud compute disks list --filter="${PVC}" --format='get(name)')
-LOCATION=$(gcloud compute disks list --filter="${PVC}" --format='value(location())')
+#PVC=$(kubectl get pvc data-sql-mariadb-master-0 -o jsonpath="{.spec.volumeName}")
+DISK=$(gcloud compute disks list  --format='get(name)') #--filter="${PVC}"
+LOCATION=$(gcloud compute disks list  --format='value(location())') #--filter="${PVC}"
 gcloud compute disks add-resource-policies ${DISK} --zone=${LOCATION} --resource-policies=catayzer-nightly-east-to-west-2d-1
