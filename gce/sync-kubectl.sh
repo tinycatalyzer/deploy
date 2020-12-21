f821460e217bb0317084bb0a2043509e1f72771b
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-20 19:56:16 -0800

    Update sync-kubectl.sh

--- gce/sync-kubectl.sh
+++ gce/sync-kubectl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-gcloud container clusters get-credentials cluster-1 --zone us-east1-b
+gcloud container clusters get-credentials cluster-1 --zone us-east1
 
 # And use the cluster we just made
 kubectl config set-cluster cluster-1
