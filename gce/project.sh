150784e10a0f52f2f2898a05eb346b65b59fdee6
tinycatalyzer <75456071+tinycatalyzer@users.noreply.github.com>
2020-12-20 19:55:58 -0800

    Update project.sh

--- gce/project.sh
+++ gce/project.sh
@@ -3,5 +3,5 @@
 # Prepare for general interaction with the cluster via gcloud in the right project and zone.
 
 # Set our defaults for the current cluster
-gcloud config set project wbstack
-gcloud config set compute/zone us-east1-b
+gcloud config set project catalyzer-wikistack
+gcloud config set compute/zone us-east1
